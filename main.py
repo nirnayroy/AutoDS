85238b6d9bd8ccf30e8f861727dffab4aa5e67e2
nirnayroy <nroy1012@gmail.com>
2021-10-21 22:45:25 +0530

    gg

---
+++ main.py
@@ -0,0 +1,11 @@
+import argparse
+
+import Data
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-source', default='Data\train.csv', help='path of data file')
+parser.add_argument('-type', default=None)
+args = parser.parse_args()
+
+data = 
