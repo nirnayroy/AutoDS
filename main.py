bd4ccb4ce865af44acc6f782f592690412013391
nirnayroy <nroy1012@gmail.com>
2021-10-26 13:38:10 +0530

    moved from csv to fetch

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import argparse
-
-import Data
+from Data import fetch
 import pandas as pd
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-source', default='Data\train.csv', help='path of data file')
+parser.add_argument('-source', default='Data/train.csv', help='path of data file')
 parser.add_argument('-type', default=None)
 args = parser.parse_args()
 
-data = pd.read_csv(r'Data\train.csv')
+data = fetch.from_csv(args.source)
 print(data.head)
