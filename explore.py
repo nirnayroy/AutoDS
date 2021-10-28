01a4527359abfdcd89ab99b97a173321ad02d690
nirnayroy <nroy1012@gmail.com>
2021-10-28 23:33:22 +0530

    print dtype funtion

--- explore.py
+++ explore.py
@@ -4,20 +4,26 @@ from sklearn.model_selection import train_test_split
 class Explorer:
     def __init__(self, data, dependentVars, ratio=0.8) -> None:
         self.data = data
-        self.categorical, self.numerical = self.separateCatNum()
         self.dependentVars = dependentVars
         self.X, self.y = self.splitXy()
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, 
                                                                                 self.y)
+        self.categorical, self.numerical = self.separateCatNum()
+        self.printOutputType()
+
+    def splitXy(self):
+        X = self.data.drop(self.dependentVars, axis=1)
+        y = self.findDependentVars()
+        return X, y
 
     def separateCatNum(self):
-        categorical = self.data.select_dtypes(include=[object]).astype("category")
-        numerical = self.data.select_dtypes(exclude=[object])
+        categorical = self.X_train.select_dtypes(include=[object]).astype("category")
+        numerical = self.X_train.select_dtypes(exclude=[object])
         return categorical, numerical
-   
+
     def findDependentVars(self):
         try:
-            depVars = self.data[self.dependentVarsdependentVars]
+            depVars = self.data[self.dependentVars]
             return depVars
         except:
             raise Exception("Dependent Variables not found")
@@ -27,27 +33,20 @@ class Explorer:
         train, test = df[msk], df[~msk]
         return train, test
     
-    def splitXy(self):
-        X = self.data.drop(self.dependentVars, axis=1)
-        y = self.data[self.dependentVars]
-        return X, y
-
-
-    
     def findCategories(self):
         for col in self.categorical:
-            print(col, ':',dataExp.categorical[col].cat.categories)
+            print(col, ':', dataExp.categorical[col].cat.categories)
+
+    def printOutputType(self):
+        print('Output Type:', self.y_train.dtypes)
 
     def printDescription(self):
-        print(self.data.describe())
+        print('Description:', self.data.describe())
 
     def printInfo(self):
-        print(self.data.info())
-
+        print('Information:', self.data.info())
 
 if __name__ == '__main__':
     from Data import fetch
     data = fetch.from_csv('Data/train.csv')
     dataExp = Explorer(data, ['SalePrice'])
-    print(len(dataExp.X))
-    print(len(dataExp.X_train))
